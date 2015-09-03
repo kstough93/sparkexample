73d03c4f6e88bdada9f081943ecc8095189fc7ef
Kevin Stough <kstough@gmail.com>
2015-09-03 11:04:46 -0400

    Adding code, pom file, and docker file

---
+++ src/main/java/sparkexample/Hello.java
@@ -0,0 +1,11 @@
+package sparkexample;
+
+import static spark.Spark.get;
+
+public class Hello 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("hello from sparkjava.com");
+    }
+}
